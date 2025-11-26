a1a85a4276966ca2af196290d6f2a80a64fec153
Avanthika <avanthikasanil6@gmail.com>
2025-11-26 13:06:39 +0530

    Add operator profile page using component

---
+++ src/app/operator/profile/page.tsx
@@ -0,0 +1,6 @@
+// File: src/app/operator/profile/page.tsx
+import OperatorProfile from "../../../components/Operator/OperatorProfile";
+
+export default function ProfilePage() {
+  return <OperatorProfile isOpen={true} onClose={() => {}} />;
+}
