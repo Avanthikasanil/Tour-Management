d7fb21d41f0a861cc57c11f6fd27e59b4e8c2af5
Avanthika <avanthikasanil6@gmail.com>
2025-11-26 14:20:23 +0530

    Fix OperatorProfile CSS import path

--- src/app/operator/dashboard/page.tsx
+++ src/app/operator/dashboard/page.tsx
@@ -6,7 +6,8 @@ import { useFormik } from "formik";
 import * as Yup from "yup";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-import styles from "./OperatorProfile.module.css";
+import styles from "../../components/Operator/OperatorProfile.module.css";
+
 
 export default function OperatorProfilePage() {
   const [initialData, setInitialData] = useState({
